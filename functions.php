69201987f1a3ec1100afb056d2c74c4b09c76577
bruce <bmpandrada@gmail.com>
2025-11-23 19:41:44 +0800

    fix: add custom queries, archive page, postpage, and while content

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function bmpa_files(): void {
     // wp_enqueue_style('bmpa_main_style', get_stylesheet_uri());
-    wp_enqueue_script( 'bmpa_main-script',get_theme_file_uri('/build/index.js'),array('jquery'),'1.0', true);
+    wp_enqueue_script( 'bmpa_main-script',get_theme_file_uri('/build/index.js'),['jquery'],'1.0', true);
     wp_enqueue_style( 'bmpa_font-google',  '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style( 'bmpa_font-awesome',  '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style( 'bmpa_main_style',  get_theme_file_uri( '/build/style-index.css'));
@@ -15,6 +15,9 @@ add_action('wp_enqueue_scripts', 'bmpa_files');
 
 
 function bmpa_features() {
+    // register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    // register_nav_menu('footerLocationNavOne', 'Footer Location Nav One');
+    // register_nav_menu('footerLocationNavTwo','Footer Location Nav Two');
     add_theme_support('title-tag');
 }
 
